66533dbbbf0808b62b29c006cd259c672729c7be
SHarber <harbers@oregonstate.edu>
2017-08-06 19:42:00 -0400

    Starting Non-built in commands

--- smallsh.c
+++ smallsh.c
@@ -1,46 +1,47 @@
 /*******************************************************************************************************
- *  Author:			Sarah Harber
+ * Author:			Sarah Harber
  * Last Update:		8/6/17
  * Description: 	Create a shell in C that will run command line instructions.
  * Specifications:	- Shell will support 3 built in commands: exit, cd, status.
- * 			- Comments will begin with '#'.
- * 			- If it is a background process & will be the last character of the command.
- * 			- A colon is the symbol as a prompt for the command line.
- * 			- Shell will allow for redirection of standard input & standard output
- * 			- Shell will support both foreground and background processes.
- * 			
+ * 					- Comments will begin with '#'.
+ * 					- If it is a background process & will be the last character of the command.
+ * 					- A colon is the symbol as a prompt for the command line.
+ * 					- Shell will allow for redirection of standard input & standard output
+ * 					- Shell will support both foreground and background processes.
+ * 					
  ******************************************************************************************************/
 
 // Set Header Files
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <sys/types.h>
+	#include <signal.h>
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include <unistd.h>
+	#include <sys/stat.h>
+	#include <fcntl.h>
+	#include <sys/wait.h>
+	#include <sys/types.h>
 
 // Declare Neccesary Variables
-	int Debug = 1; // Debugging Variable.
-	char buffer[2048]; // Character Array Buffer
-	char *b = buffer; // Pointer to Buffer
-	size_t bufsize = 512; // Size_t of Buffer size
-	size_t arguments; //Arguments temp holds buffer
-	int toexit = 0; // Int Variable to exit.
-	pid_t bgpid[100]; // An array to hold process ids of background processes.
-	int num_bg_Processes = 0; // Number of background Processes.  
-	pid_t lastFGProcessPID; // Variable to hold last fg process.
-	char args[512]; // Variable to store arguments;
-
+	int Debug = 0; 				// Debugging Variable.
+	char buffer[2048]; 			// Character Array Buffer
+	char *b = buffer; 			// Pointer to Buffer
+	size_t bufsize = 512; 		// Size_t of Buffer size
+	size_t arguments; 			// Arguments temp holds buffer
+	int toexit = 0; 			// Int Variable to exit.
+	pid_t bgpid[100]; 			// An array to hold process ids of background processes.
+	int num_bg_Processes = 0; 	// Number of background Processes.  
+	pid_t lastFGProcessPID; 	// Variable to hold last fg process.
+	char args[512]; 			// Variable to store arguments;
+	int numberOfArguments; 		// Variable to store the current number of arguments.
 
 // Declare function definitions
-void getInput(); // Display command line & get input.
-void builtInCommands(); // Check to see if command is built in & execut if it is.
+	void getInput(); 			// Display command line & get input.
+	void builtInCommands(); 	// Check to see if command is built in & execut if it is.
+	void otherCommands(); 		// All other Commands will be handled. 
 
 /****************************************************************
- *			  MAIN FUNCTION
+ *			  			 MAIN FUNCTION
  ***************************************************************/
  int main()
  {
@@ -52,17 +53,17 @@ void builtInCommands(); // Check to see if command is built in & execut if it is
  }
 
  /******************************************************************************************************
- *				Define Functions Declared Above
+ *				 					Define Functions Declared Above
  ******************************************************************************************************/
 
 
 /****************************************************************
  * Function Name: 	getInput
  * Description: 	Function to display the command line and
- *			get user input.
+ *					get user input.
  * Specifications:	If user inputs a comment (line that begins
- * 			with '#' or a blank line, the command line
- * 			will reprompt the user for a new command.
+ * 					with '#' or a blank line, the command line
+ * 					will reprompt the user for a new command.
  **************************************************************/
 void getInput()
 {
@@ -97,14 +98,16 @@ void getInput()
 	}while(buffer[0] == '\n' || buffer[0] == '#');
 
 	// Split buffer into tokens & store in arg 
-	char * token; // Set char pointer to hold tokens temporarily.
-	int numberOfArguments = 0; // Set number of Arugments to 0
-	token = strtok(buffer, " \n"); // Get First Token (Will not be null)
-	
+		char * token; // Set char pointer to hold tokens temporarily.
+		token = strtok(buffer, " \n"); // Get First Token (Will not be null)
+		numOfArguments = 0; // Reset number of Arguments to 0.
+
+	// While There are still tokens, store the current token and get the next.	
 	while(token != NULL && numberOfArguments < 512)
 	{
 		args[numberOfArguments] = *token;
-		printf("token %d: %s \n", numberOfArguments, token);
+		if(Debug ==1)
+			printf("token %d: %s \n", numberOfArguments, token);
 		numberOfArguments++;
 		token=strtok(NULL, "  \n");
 	}	
@@ -117,12 +120,12 @@ void getInput()
  * Function Name: 	builtInCommands
  * Description: 	Function to handle 3 built in commands. 
  * Specifications:	Fuction will handle exit, status and cd.  
- * 			Exit will exit the shell. cd will change
- * 			directories.  Without arguments, it will
- * 			change to the HOME environment.  Status
- * 			will print out either the exit status or
- * 			the terminating signal of the last 
- * 			foreground process.
+ * 					Exit will exit the shell. cd will change
+ * 					directories.  Without arguments, it will
+ * 					change to the HOME environment.  Status
+ * 					will print out either the exit status or
+ * 					the terminating signal of the last 
+ * 					foreground process.
  **************************************************************/
 void builtInCommands()
 {
@@ -141,4 +144,20 @@ void builtInCommands()
 		
 
 	}
+	else 
+		otherCommands();
 }
+
+
+/***************************************************************
+ * Function Name: 	otherCommands
+ * Description: 	Function to nonBuiltInCommands 
+ * Specifications:	Fuction will handle exit, status and cd.  
+ **************************************************************/
+void otherCommands()
+{
+
+
+}
+
+
