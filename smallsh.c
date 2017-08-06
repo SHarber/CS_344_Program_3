069a6db0182cf9d6fb75f7bf7172b9fc71a564cc
SHarber <harbers@oregonstate.edu>
2017-08-06 13:41:03 -0400

    Shell Of Shellsh.c

---
+++ smallsh.c
@@ -0,0 +1,50 @@
+/*******************************************************************************************************
+ * Author:		Sarah Harber
+ * Last Update:		8/6/17
+ * Description: 	Create a shell in C that will run command line instructions.
+ * Specifications:	- Shell will support 3 built in commands: exit, cd, status.
+ * 			- Comments will begin with '#'.
+ * 			- If it is a background process & will be the last character of the command.
+ * 			- A colon is the symbol as a prompt for the command line.
+ ******************************************************************************************************/
+
+// Set Header Files
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+// Declare Neccesary Variables
+
+
+
+// Declare function definitions
+
+
+
+/****************************************************************
+ * 			MAIN FUNCTION
+ ***************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+ /******************************************************************************************************
+ * 					Define Functions Declared Above
+ ******************************************************************************************************/
+
+
+
+
